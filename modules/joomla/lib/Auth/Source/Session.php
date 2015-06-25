1cd57a8a7612c91eb5725dece60242d4a0d31b9b
Stefano <Stefano@GodBless.homenet.telecomitalia.it>
2015-06-25 20:47:55 +0200

    asd

--- modules/joomla/lib/Auth/Source/Session.php
+++ modules/joomla/lib/Auth/Source/Session.php
@@ -105,8 +105,8 @@ class sspmod_joomla_Auth_Source_Session extends SimpleSAML_Auth_Source {
             $usermess = JText::_($langHandle);
             if ($usermess == $langHandle) {
                 $usermess = "";
-            }else{
-            	$app->enqueueMessage($usermess);
+            } else {
+                $app->enqueueMessage($usermess);
             }
             // die($usermess);
             jimport('joomla.application.component.helper');
@@ -122,11 +122,14 @@ class sspmod_joomla_Auth_Source_Session extends SimpleSAML_Auth_Source {
               $link = JURI::root();
               } */
             //   die($returnTo);
-            jimport( 'joomla.methods' );
-            $link = "index.php?option=com_users&view=login&return=" . base64_encode($returnTo); //NO FIRST SALSH OR NOT ROUTED TO DEF MENU
-           
-           // echo $link; echo " ... routed: ";
-         //   $link="index.php?option=com_users&view=login";
+            jimport('joomla.methods');
+            JFactory::getApplication()->setUserState('users.login.form.data', array("return" => $returnTo)); //good but the default joomla view seems to ignore it, it only want a POST
+            JFactory::getSession()->close();
+            $link = "index.php?option=com_users&view=login&return=" . base64_encode($returnTo);
+
+//NO FIRST SALSH OR NOT ROUTED TO DEF MENU
+            // echo $link; echo " ... routed: ";
+            //   $link="index.php?option=com_users&view=login";
             $rediBase = JRoute::_($link);
 //echo $rediBase; die();
             $rediURL = JRoute::_($rediBase, false);
@@ -138,42 +141,76 @@ class sspmod_joomla_Auth_Source_Session extends SimpleSAML_Auth_Source {
         } else {
             $state['Attributes']['email'][] = $juser->get("email");
             $state['Attributes']['eduPersonPrincipalName'][] = $juser->get("email");
-            $fullName=$juser->get("name");
+            $fullName = $juser->get("name");
             $state['Attributes']['cn'][] = $fullName;
             //TODO: use the samlogin profile plugin as joomla only got fullname
-            $firstName=$fullName;
-            $lastName=$fullName;
-            
-            $nameparts=  explode(" ", $fullName);
-            if ($nameparts[0]){
-               $firstName  = $nameparts[0];
+            $firstName = $fullName;
+            $lastName = $fullName;
+
+            $nameparts = explode(" ", $fullName);
+            if ($nameparts[0]) {
+                $firstName = $nameparts[0];
             }
-            if (isset($nameparts[1])){
-                   $lastName  = $nameparts[1];
+            if (isset($nameparts[1])) {
+                $lastName = $nameparts[1];
             }
-            
+
             //TODO: use the samlogin profile plugin
-            $state['Attributes']['givenName'][]=$firstName;
+            $state['Attributes']['givenName'][] = $firstName;
             $state['Attributes']['sn'][] = $lastName;
-           
+
+            //tableau server
+            $state['Attributes']['username'][] = $juser->get("username");
+
             //start emulate OneLogin attrs
             $state['Attributes']['User.Username'][] = $juser->get("email");
             $state['Attributes']['User.FirstName'][] = $firstName;
             $state['Attributes']['User.LastName'][] = $lastName;
-            $state['Attributes']['User.email'][] =  $juser->get("email");
-            $groupNames=array();
-            foreach ($juser->get("groups") as $groupId => $value){
+            $state['Attributes']['User.email'][] = $juser->get("email");
+            $groupNames = array();
+            foreach ($juser->get("groups") as $groupId => $value) {
                 $db = JFactory::getDbo();
                 $db->setQuery(
-                    'SELECT `title`' .
-                    ' FROM `#__usergroups`' .
-                    ' WHERE `id` = '. (int) $groupId
+                        'SELECT `title`' .
+                        ' FROM `#__usergroups`' .
+                        ' WHERE `id` = ' . (int) $groupId
                 );
-                $groupNames[]= $db->loadResult();
-                
+                $groupNames[] = $db->loadResult();
             }
+            $userid = $juser->get("id");
+            //$state['Attributes']["User.id"] = array($userid);
             $state['Attributes']['User.jGroupNames'] = $groupNames;
             $state['Attributes']['User.jGroupIDs'] = $juser->get("groups");
+            try {
+
+
+                $db->setQuery(
+                        'SELECT `person_number`,`user_id`,`first_name`,`last_name`' .
+                        ' FROM `bwp_user`' .
+                        ' WHERE `jos_id` = ' . (int) $userid
+                );
+                $bwp_user = $db->loadAssoc();
+
+
+                $db->setQuery(
+                        'SELECT `bwp_accountId`' .
+                        ' FROM `bwp_premises_accounts`' .
+                        ' WHERE `user_id` = ' . (int) $bwp_user["user_id"]
+                );
+                $bwpAccountId = $db->loadResult();
+            
+                $state['Attributes']['givenName'] = array($bwp_user["first_name"]);
+                $state['Attributes']['sn'] = array($bwp_user["last_name"]);;
+                
+                $state['Attributes']['personid'] = array($bwp_user["person_number"]);
+                // $state['Attributes']['user_id']=array($bwp_user["user_id"]);
+                $state['Attributes']['accountid'] = array($bwpAccountId);
+
+                //TODO configurable SQL mappings
+                //$samloginParams->get("attrmapping_")
+            } catch (Exception $noExtraSQL) {
+                
+            }
         }
         return $state;
     }
@@ -224,34 +261,33 @@ class sspmod_joomla_Auth_Source_Session extends SimpleSAML_Auth_Source {
          */
         assert('FALSE');
     }
-    
-    
-    	/**
-	 * This function is called when the user start a logout operation, for example
-	 * by logging out of a SP that supports single logout.
-	 *
-	 * @param array &$state  The logout state array.
-	 */
-	public function logout(&$state) {
-		assert('is_array($state)');
-                $app = self::_getJoomlaApp('site', 8);
-                //TODO: check if case to set no duplicate logout on bridged situation
-                $app->logout(); 
-		if (!session_id()) {
-			/* session_start not called before. Do it here. */
-			session_start();
-		}
 
-		/*
-		 * In this example we simply remove the 'uid' from the session.
-		 */
-		unset($_SESSION['uid']);
+    /**
+     * This function is called when the user start a logout operation, for example
+     * by logging out of a SP that supports single logout.
+     *
+     * @param array &$state  The logout state array.
+     */
+    public function logout(&$state) {
+        assert('is_array($state)');
+        $app = self::_getJoomlaApp('site', 8);
+        //TODO: check if case to set no duplicate logout on bridged situation
+        $app->logout();
+        if (!session_id()) {
+            /* session_start not called before. Do it here. */
+            session_start();
+        }
 
-		/*
-		 * If we need to do a redirect to a different page, we could do this
-		 * here, but in this example we don't need to do this.
-		 */
-	}
+        /*
+         * In this example we simply remove the 'uid' from the session.
+         */
+        unset($_SESSION['uid']);
+
+        /*
+         * If we need to do a redirect to a different page, we could do this
+         * here, but in this example we don't need to do this.
+         */
+    }
 
 }
 
